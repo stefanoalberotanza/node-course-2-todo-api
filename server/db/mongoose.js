a6f024c6800839c3c7d8e677b4f9deb0396cbd84
stefalber <stefalber@gmail.com>
2018-04-03 21:46:34 +0200

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -6,18 +6,10 @@ var connectPath, options;
 //Check if we are on Heroku
 if(process.env.PORT){
  connectPath = process.env.MONGODB_URI;
- options= {
-     auth: {
-         user: '<dbuser>',
-         password: '<dbpassword>'
-     }
- }
 }else{
  connectPath = "mongodb://localhost:27017/TodoApp";
- options = {}
 }
-mongoose.connect(connectPath, options);
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(connectPath);
 
 
 module.exports = {mongoose};
