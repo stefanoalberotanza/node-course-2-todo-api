9ce76bcc0d2479974e094237c6b787a44647bf3d
stefalber <stefalber@gmail.com>
2018-04-05 01:36:54 +0200

    Setup separate test and development anvs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,15 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-
-var connectPath, options;
-//Check if we are on Heroku
-if(process.env.PORT){
- connectPath = process.env.MONGODB_URI;
-}else{
- connectPath = "mongodb://localhost:27017/TodoApp";
-}
-mongoose.connect(connectPath);
+mongoose.connect(process.env.MONGODB_URI);
 
   
 module.exports = {mongoose};
